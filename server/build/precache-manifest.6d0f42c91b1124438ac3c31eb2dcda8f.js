abbfaf7d96bc531f5bce6117e01dce8467eb1f1d
Vamc1432 <vamshipk5@gmail.com>
2022-08-19 12:04:58 +0530

    heroku fuxes 4

---
+++ server/build/precache-manifest.6d0f42c91b1124438ac3c31eb2dcda8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f637e1fec2606c264a38dcba4641f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "572790f275b2dd28d14a",
+    "url": "/static/js/2.97874bae.chunk.js"
+  },
+  {
+    "revision": "219953b2c8eb3bbcd261",
+    "url": "/static/js/main.6bc00999.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
@@ -12,6 +13,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 const keeperSchema = {
     Ntitle: String,
